40f136be3c16c8d3cb1a7f504a07da72af1731a0
sleek1004 <arojojoyeo@yahoo.com>
2022-10-15 23:47:24 +0100

    airbnb in the making

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Airlogo from './images/Airlogo.png'
+import airpics from './images/airpics.png'
+
+
+function Navbar(){
+  return(
+    <nav>
+    <img src={Airlogo} alt="images/Airlogo.png" className='logo' />
+    </nav>
+  )
+}
+function Firstcontent(){
+  return(
+    <section className='hero'>
+     <img src={airpics} alt="images/airpics.png" className='logo2' />
+     <h1>Online Experiences</h1>
+     <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+    </section>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div>
+<Navbar/>
+<Firstcontent />
     </div>
   );
 }
